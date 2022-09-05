6aa8d49aca68696a327ccc40eaf3919dfe62bd78
JuanMaLR <A01206568@itesm.mx>
2022-09-05 17:06:02 -0500

    Commit inicial

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 'c1vjRnm!5bXg3pTn6Jcvo^n6U#Ks&p0X!xM6SXaPIdl^ig0YdneAVO',
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
